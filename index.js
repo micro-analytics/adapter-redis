323bd4ce514379532f58b9d9ef8f08539b7da251
Rolf Erik Lekang <me@rolflekang.com>
2017-01-11 20:04:29 +0100

    fix: Correctly parse json

--- index.js
+++ index.js
@@ -5,15 +5,26 @@ var redis = new Redis();
 var HASH_KEY = 'micro-analytics';
 
 function get(key) {
-  return redis.hget(HASH_KEY, key);
+  return redis.hget(HASH_KEY, key)
+    .then((value) => JSON.parse(value));
 }
 
 function put(key, value) {
-  return redis.hset(HASH_KEY, key, value);
+  return redis.hset(HASH_KEY, key, JSON.stringify(value));
 }
 
 function getAll(options) {
-  return redis.hgetall(HASH_KEY);
+  return redis.hgetall(HASH_KEY)
+    .then((value) =>
+      Object.keys(value)
+      .reduce((lastValue, item) =>
+        Object.assign(
+          {},
+          lastValue,
+          { [item]: JSON.parse(value[item])}
+        )
+      )
+    );
 }
 
 function has(key) {
