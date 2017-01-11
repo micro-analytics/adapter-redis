f066f9de1055bd82a74842071bf737bdd170bfb6
Rolf Erik Lekang <me@rolflekang.com>
2017-01-11 14:54:56 +0100

    feat: Add initial implementation

---
+++ index.js
@@ -0,0 +1,28 @@
+var Redis = require('ioredis');
+
+var redis = new Redis();
+
+var HASH_KEY = 'micro-analytics';
+
+function get(key) {
+  return redis.hget(HASH_KEY, key);
+}
+
+function put(key, value) {
+  return redis.hset(HASH_KEY, key, set);
+}
+
+function getAll(options) {
+  return redis.hgetall(HASH_KEY);
+}
+
+function has(key) {
+  return redis.hexists(HASH_KEY, key);
+}
+
+module.exports = {
+  get: get,
+  put: put,
+  has: has,
+  getAll: getAll,
+}
