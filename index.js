85ba425656e4c2bea5fe799078f5f345bcb5f7eb
Jim Cummins <jimthedev@gmail.com>
2017-03-21 10:59:02 -0500

    feat: Add configuration of redis connection
    Fixes #4

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const Redis = require('ioredis');
 
-const redis = new Redis();
+const DB_CONFIG_RAW = process.env.MAA_REDIS_DB_CONFIG;
+
+let DB_CONFIG;
+
+if(DB_CONFIG_RAW) {
+  try {
+    DB_CONFIG = JSON.parse(DB_CONFIG_RAW);
+  } catch(e) {
+    DB_CONFIG = DB_CONFIG_RAW;
+  }
+}
+
+const redis = new Redis(DB_CONFIG);
 
 const HASH_KEY = process.env.MAA_REDIS_HASH_KEY || 'micro-analytics';
 
