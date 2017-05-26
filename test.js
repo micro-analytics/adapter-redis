835a5802e67edde00402142b3a0a857fbb90b6b7
Rolf Erik Lekang <me@rolflekang.com>
2017-05-26 19:47:02 +0200

    feat: Add support for filtering (#15)
    * chore: Upgrade jest and micro-analytics-cli
    
    * feat: Add support for filtering
    
    Fixes #14

--- test.js
+++ test.js
@@ -1,15 +1,15 @@
-const Redis = require('ioredis')
-const test = require('micro-analytics-cli/adapter-tests/unit-tests')
-const path = require('path')
+const Redis = require('ioredis');
+const test = require('micro-analytics-cli/adapter-tests/unit-tests');
+const path = require('path');
 
 const DB_CONFIG_RAW = process.env.MAA_REDIS_DB_CONFIG;
 
 let DB_CONFIG;
 
-if(DB_CONFIG_RAW) {
+if (DB_CONFIG_RAW) {
   try {
     DB_CONFIG = JSON.parse(DB_CONFIG_RAW);
-  } catch(e) {
+  } catch (e) {
     DB_CONFIG = DB_CONFIG_RAW;
   }
 }
@@ -18,14 +18,13 @@ const db = new Redis(DB_CONFIG);
 
 const HASH_KEY = process.env.MAA_REDIS_HASH_KEY || 'micro-analytics';
 
-
 test({
   name: 'redis',
   modulePath: path.resolve(__dirname, './index.js'),
   beforeEach: async () => {
-    await db.flushall()
+    await db.flushall();
   },
-  afterAll: async (adapter) => {
-    await db.disconnect()
+  afterAll: async adapter => {
+    await db.disconnect();
   }
-})
+});
